cbc7567dba7afe8871164608c355d01fca39a738
Minh-Tuan Ta <minhtuan.ta.v@gmail.com>
2018-05-13 15:45:04 -0500

    feat: Add styling for small screen

--- js/main.js
+++ js/main.js
@@ -141,7 +141,11 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = restaurant.name;
   li.append(image);
+
+  const restaurantDetails = document.createElement('div');
+  restaurantDetails.className = 'restaurant-details';
 
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
@@ -158,7 +162,13 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+
+  restaurantDetails.appendChild(name);
+  restaurantDetails.appendChild(neighborhood);
+  restaurantDetails.appendChild(address);
+  restaurantDetails.appendChild(more);
+
+  li.append(restaurantDetails);
 
   return li
 }
